5e47a117ba7715c77cbbdf88b70394e3b7e2f0d6
Sangeeth Mathew John <65032296+sangethmathewjohn@users.noreply.github.com>
2021-06-19 23:20:31 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,34 @@
+import random
+
+ans = random.randint(1,100)
+
+def check(cguess,guess):
+    if guess>cguess:
+        print("Too High")
+        return True
+    elif guess<cguess:
+        print("Too Low")
+        return True
+    elif guess==cguess:
+        print(f"Good you found the number {cguess} ")
+        return False  
+
+def level(difficulty):
+    for _ in range(difficulty):
+        guess=int(input("Make your guess: "))
+        while not check(ans,guess):
+            exit()
+   
+print("Welcome to the number Guesisng Game!")
+print("I'm thinking of a number between 1 and 100")
+
+difficulty = input("Choose difficulty level easy, medium or hard  :")
+
+if difficulty=="easy":
+    diffy =10
+elif difficulty=="medium":
+    diffy=7
+else:
+    diffy=5
+    
+level(diffy)   
